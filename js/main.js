167d2f5bb392387e4d22e1db73fa5bbf384b754c
swag31415 <kidswagatam18@gmail.com>
2021-12-21 21:36:28 -0500

    Added function to get books

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const cache = {}
+async function get_book(book) {
+  if (cache[book]) return cache[book]
+  res = await fetch('/data/'+book+'.json')
+  if (!res.ok) throw new Error(res.statusText)
+  data = await res.text()
+  cache[book] = JSON.parse(data)
+  return cache[book]
+}
+
 const params = new URLSearchParams(window.location.search)
 let book = params.get('book')
 let options = new Set(params.getAll('option'))
