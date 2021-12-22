6c9ea711389d043ee2d7d5b8fea4aea67c2fa130
swag31415 <kidswagatam18@gmail.com>
2021-12-21 22:48:30 -0500

    Added accounting for options

--- js/main.js
+++ js/main.js
@@ -9,32 +9,44 @@ async function get_book(book) {
 }
 
 const brk = s => s.trim().replaceAll('\n', '</br>')
-const get_html = async (data) => ejs.render(`
+const get_html = async (data, options) => ejs.render(`
 <% data.forEach(book => { %>
-  <h2><%- brk(book.name) %></h2>
+  <% if (options.has('name')) { %>
+    <h2><%- brk(book.name) %></h2>
+  <% } %>
   <% book.chapters.forEach(chapter => { %>
-    <h3 class="center-align"><%- brk(chapter.name) %></h3>
-    <% if (chapter.desc) { %>
+    <% if (options.has('title')) { %>
+      <h3 class="center-align"><%- brk(chapter.name) %></h3>
+    <% } %>
+    <% if (options.has('desc') && chapter.desc) { %>
       <p><%- brk(chapter.desc) %></p>
     <% } %>
     <% chapter.verses.forEach(verse => { %>
-      <h4><%- brk(verse.verse) %></h4>
-      <% if (verse.devanagari || verse.bengali) { %>
+      <% if (options.has('numbers')) { %>
+        <h4><%- brk(verse.verse) %></h4>
+      <% } %>
+      <% if (options.has('og') && (verse.devanagari || verse.bengali)) { %>
         <p class="center-align bold"><%- brk(verse.devanagari || verse.bengali) %></p>
       <% } %>
-      <p class="center-align"><%- brk(verse['verse-text']) %></p>
-      <h5 class="center-align">Synonyms</h5>
-      <p><%- brk(verse.synonyms) %></p>
-      <h5 class="center-align">Translation</h5>
-      <p class="bold"><%- brk(verse.translation) %></p>
-      <% if (verse.purport) { %>
+      <% if (options.has('verse')) { %>
+        <p class="center-align"><%- brk(verse['verse-text']) %></p>
+      <% } %>
+      <% if (options.has('synonyms')) { %>
+        <h5 class="center-align">Synonyms</h5>
+        <p><%- brk(verse.synonyms) %></p>
+      <% } %>
+      <% if (options.has('translation')) { %>
+        <h5 class="center-align">Translation</h5>
+        <p class="bold"><%- brk(verse.translation) %></p>
+      <% } %>
+      <% if (options.has('purport') && verse.purport) { %>
         <h5 class="center-align">Purport</h5>
         <p><%- brk(verse.purport) %></p>
       <% } %>
     <% }) %>
   <% }) %>
 <% }) %>
-`, {data: data})
+`, {data: data, options: options})
 
 const params = new URLSearchParams(window.location.search)
 let book = params.get('book')
@@ -44,7 +56,7 @@ if (book && options) {
   book_select.value = book
   let option_select_options = [...document.querySelector(`#search select[name=option]`).options]
   option_select_options.forEach(option => option.selected = options.has(option.value))
-  get_book(book).then(data => get_html(data)).then(html => {
+  get_book(book).then(data => get_html(data, options)).then(html => {
     document.getElementById('content').innerHTML = html
   }).catch(err => {
     console.error(err)
